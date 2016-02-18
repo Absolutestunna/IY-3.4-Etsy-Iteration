491a589bba414bdd41c2094432646567107fc1df
Joshua Abu <joshujah007@gmail.com>
2016-02-18 16:02:27 -0500

    second problem

--- main.js
+++ main.js
@@ -10,13 +10,3 @@ var sel_prices = 0;
     // console.log("The average is: " + (sel_prices/(items.length)));
 }
 itemPriceAvg();
-
-
-////////////////////////////////////////
-//Range between numbers
-////////////////////////////////////////
-function itemsBetween(value){
-  // console.log(value.price)
-  return (value.price >= 14 && value.price <=18);
-}
-console.log ("Items that cost between $14.00 USD and $18.00 USD:" + (items.filter(itemsBetween)));
